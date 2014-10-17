f62fb78bce3d198111bcd22d0e3a461e6b7350e4
Piotr Lizończyk <piotr.lizonczyk@deployed.pl>
2014-10-17 22:51:24 +0200

    asdasd

--- is_available.py
+++ is_available.py
@@ -1,11 +1,11 @@
 def is_available(move=None, snake1=None, snake2=None, food=None, data=None, board_width=None, board_height=None):
-    unavailable_points = [snake1.head, snake2.head]
-    unavailable_points.append(snake1.body)
-    unavailable_points.append(snake2.body)
-    unavailable_points.append([Point(x=i, y=board_height) for i in xrange(board_width)])
-    unavailable_points.append([Point(y=i, x=board_width) for i in xrange(board_height)])
-    unavailable_points.append([Point(x=i, y=0) for i in xrange(board_width)])
-    unavailable_points.append([Point(y=i, x=0) for i in xrange(board_height)])
+    unavailable_points = []
+    unavailable_points.extend(snake1.body)
+    unavailable_points.extend(snake2.body)
+    unavailable_points.extend([Point(x=i, y=board_height) for i in xrange(board_width)])
+    unavailable_points.extend([Point(y=i, x=board_width) for i in xrange(board_height)])
+    unavailable_points.extend([Point(x=i, y=0) for i in xrange(board_width)])
+    unavailable_points.extend([Point(y=i, x=0) for i in xrange(board_height)])
 
     if move == 'd' and Point(x=snake1.head.x, y=snake1.head.y+1) in unavailable_points:
         return False
